c0ca7d7e4518ce73bdc976f2cf4b35816d8ffa52
Anastasiya-Shir <86313786+Anastasiya-Shir@users.noreply.github.com>
2022-06-03 10:56:05 +0300

    fixed

--- API/index.js
+++ API/index.js
@@ -5,47 +5,53 @@ let inputValue;
 function imgCreate(data) {
   for (let i = 0; i < data.results.length; i++) {
     let img = document.createElement("img");
+
     img.className = "picture";
+
     img.src = data.results[i].links.download;
+
     imgContainer.append(img);
   }
 }
 
 input.addEventListener("keydown", (event) => {
-
   if (event.key === "Enter") {
     document.querySelector(".photo").textContent = "";
 
     inputValue = input.value;
 
-    let promise = fetch('https://api.unsplash.com/search/photos?query=' + input.value + '&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo')
-
+    fetch('https://api.unsplash.com/search/photos?query=' + input.value + '&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo')
       .then(response => {
         return response.json();
       })
-
       .then(data => {
         imgCreate(data);
       })
-
       .catch(error => console.log(error));
   }
 });
 
 
 function imgCreate(data) {
-  if (data.results.length == 0) {
-    let p = document.createElement("p");
-    p.className = "picture";
-    p.innerHTML = "Таких картинок нет"
-    p.style.color = "white"
-    imgContainer.append(p);
+  if (!data.results.length) {
+    let textIfPictureDidNotFind = document.createElement("p");
+
+    textIfPictureDidNotFind.className = "picture";
+
+    textIfPictureDidNotFind.innerHTML = "Таких картинок нет";
+
+    textIfPictureDidNotFind.style.color = "white";
+
+    imgContainer.append(textIfPictureDidNotFind);
   }
 
   for (let i = 0; i < data.results.length; i++) {
     let img = document.createElement("img");
+
     img.className = "picture";
+
     img.src = data.results[i].links.download;
+
     imgContainer.append(img);
   }
 }
